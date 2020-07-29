f14b3869fee0cac87831ce4a55afd2ca277818bb
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-29 17:28:48 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,52 @@
-class Owner
-  # code goes here
-end
+class Owner 
+  attr_reader:species, :name, :pets 
+  
+  @@owner = []
+  @@pets = {:dogs => [], :cats => []}
+  
+  def initialize(name)
+    @species= "human"
+    @name = name 
+    @@owner << self 
+  end 
+  
+  def say_species 
+    "I am a #{@species}."
+  end 
+  
+  def self.all 
+    @@owner
+  end 
+  
+  def self.count 
+    @@owner.size 
+  end 
+  
+  def self.reset_all 
+    @@owner.clear
+  end 
+  
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(cat_name)
+    @@pets[:cats] << Cat.new(cat_name, self)
+  end
+  
+  def buy_dog(dog_name)
+    @@pets[:dogs] << Dog.new(dog_name, self)
+  end
+
+  def walk_dogs
+    @@pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+end 
+
+
